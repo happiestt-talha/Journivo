c403cf08a5ace935a5c5a271f9e850029d4cca7e
Talha <mtalhamanzoor786@gmail.com>
2024-08-09 00:20:23 +0500

    Completed Navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,57 @@
-import { Navbar, TextInput } from 'flowbite-react'
-import React from 'react'
+import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
 import logo from '../assets/icons/blog-50.png'
-import { Link } from 'react-router-dom'
-const Header = () => {
+
+export default function Header() {
+    const path = useLocation().pathname;
     return (
-        <>
-            <Navbar className='border-b-2'>
-                <Link to="/"
-                    className="flex items-center"
-                >
-                    <img src={logo} alt="Journivo" />
+        <Navbar className='border-b-2'>
+            <Link to="/" className="flex items-center">
+                <img src={logo} alt="Journivo" />
 
-                    <span
-                        className="self-center whitespace-nowrap text-xl font-semibold
+                <span
+                    className="self-center whitespace-nowrap text-xl font-semibold
                         bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-1
-                        text-white rounded-lg dark:text-white"
-                    >Journivo</span>
+                        text-white rounded-lg dark:text-white">
+                    Journivo</span>
 
-                    <span
-                        className='font-bold text-xl'
-                    >'s Blog</span>
-                </Link>
-
+                <span
+                    className='font-bold text-xl'
+                >'s Blog</span>
+            </Link>
+            <form>
                 <TextInput
-                    type="search"
-                    placeholder="Search..."
-                    className="w-80"
+                    type='text'
+                    placeholder='Search...'
+                    rightIcon={AiOutlineSearch}
+                    className='hidden lg:inline'
                 />
-            </Navbar>
-        </>
-    )
+            </form>
+            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+                <AiOutlineSearch />
+            </Button>
+            <div className='flex gap-2 md:order-2'>
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+                    <FaMoon />
+                </Button>
+                <Link to='/sign-in'>
+                    <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+                </Link>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link active={path === "/"} as={'div'}>
+                    <Link to='/'>Home</Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/about"} as={'div'}>
+                    <Link to='/about'>About</Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === "/projects"} as={'div'}>
+                    <Link to='/projects'>Projects</Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
-
-export default Header
