f034e8e48b036ef341a7db1c3ece90be1f99e177
Talha <mtalhamanzoor786@gmail.com>
2024-08-11 21:06:14 +0500

    UI fixed

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -8,7 +8,7 @@ const Signin = () => {
             <div className='max-h-screen'>
                 <div className='mt-10 flex p-3 max-w-4xl mx-auto flex-col md:flex-row md:items-center gap-5'>
 
-                    <div className='flex-1 '>
+                    <div className='flex-1 order-2 md:order-1'>
 
                         <form className='flex flex-col gap-5'>
                             <div>
@@ -29,7 +29,7 @@ const Signin = () => {
 
                     </div>
 
-                    <div className='flex-1 '>
+                    <div className='flex-1 order-1 md:order-2'>
                         <Link to="/" className="flex items-center font-bold text-4xl dark:text-white">
                             <span
                                 className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-1 text-white rounded-lg dark:text-white">
