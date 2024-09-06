db4cd123207ed0033f714c52b990fd782e5b69e2
Talha <mtalhamanzoor786@gmail.com>
2024-09-06 22:30:09 +0500

    Added recent articles

--- client/src/posts/Post.jsx
+++ client/src/posts/Post.jsx
@@ -5,12 +5,26 @@ import { Button } from "flowbite-react"
 import Loader from "../components/Loader"
 import CallToAction from "../components/CallToAction"
 import CommentSection from "../components/CommentSection"
+import PostCard from "../components/PostCard"
 const Post = () => {
     const { slug } = useParams()
     const [post, setPost] = useState({})
+    const [recentArticles, setRecentArticles] = useState([])
     const [author, setAuthor] = useState({})
     const [loading, setLoading] = useState(true)
 
+    useEffect(() => {
+        const getRecentlyPosted = async () => {
+            try {
+                const res = await axios.get('/post/getpost?limit=3')
+                setRecentArticles(res.data.posts)
+            }
+            catch (error) {
+                console.log("Error: ", error.response.data.message)
+            }
+        }
+        getRecentlyPosted()
+    }, [])
     useEffect(() => {
         try {
             const getData = async () => {
@@ -64,6 +78,23 @@ const Post = () => {
                                 <CallToAction />
                             </div>
                             <CommentSection postId={post._id} />
+
+                            <div>
+                                {
+                                    recentArticles.length > 0
+                                        ? <div className="max-w-4xl mx-auto my-8">
+                                            <h3 className="text-2xl font-bold mb-4">Recent Articles</h3>
+                                            <div className="flex flex-wrap gap-4 items-center justify-center">
+                                                {
+                                                    recentArticles.map((article) => (
+                                                        <PostCard key={article._id} post={article} />
+                                                    ))
+                                                }
+                                            </div>
+                                        </div>
+                                        : null
+                                }
+                            </div>
                         </div>
 
                     </>
